ca901d21dd2fabf860cc51214c93a23678dda5e2
Joao Paulo Polles <joaopaulopolles@gmail.com>
2017-05-09 15:55:40 -0300

    Alterações ainda não finalizadas

--- app/Http/Controllers/InscricaoController.php
+++ app/Http/Controllers/InscricaoController.php
@@ -33,7 +33,7 @@ class InscricaoController extends Controller
    *  @param  integer numero [0-5] => number of Person's Address (nullable)
    *  @param  string  bairro [0-40] => zone of Person's Address (nullable)
    *  @param  string  cidade [1-100] => city of Person's Address
-   *  @param  string  uf [20] => state of Person's Address
+   *  @param  string  uf [2] => state of Person's Address
    *  @param  string  telefone1 [11] => Phone 1 of Person
    *  @param  string  telefone2 [0 || 11] => Phone 2 of Person (nullable)
    *  @param  string  instituicao [0-100] => College of Person (nullable)
@@ -43,35 +43,35 @@ class InscricaoController extends Controller
   public function postNew(Request $request){
 
     // Faz a validação dos dados
-    $this->validate($request, [
-      'nome' => 'required|string|min:5|max:60',
-      'cpf' => 'required|string|min:14|max:14|unique:Pessoa,Cpf',
-      'email' => 'required|string|min:5|max:100',
-      'cep' => 'required|string|min:9|max:9',
-      'logradouro' => 'required|string|max:100',
-      'numero' => 'string|max:5',
-      'bairro' => 'string|max:40',
-      'cidade' => 'required|string|min:1|max:100',
-      'uf' => 'required|string|min:2|max:2',
-      'telefone1' => 'required|string|min:11|max:11',
-      'telefone2' => 'string|min:11|max:11',
-      'instituicao' => 'string|max:100',
-      'curso' => 'string|max:50',
-      'alunoUnivel' => 'required|int|min:1|max:1'
-    ]);
+    // $this->validate($request, [
+    //   'nome' => 'required|string|min:5|max:60',
+    //   'cpf' => 'required|string|min:14|max:14|unique:Pessoa,Cpf',
+    //   'email' => 'required|string|min:5|max:100',
+    //   'cep' => 'required|string|min:9|max:9',
+    //   'logradouro' => 'required|string|max:100',
+    //   'numero' => 'string|max:5',
+    //   'bairro' => 'string|max:40',
+    //   'cidade' => 'required|string|min:1|max:100',
+    //   'uf' => 'required|string|min:2|max:2',
+    //   'telefone1' => 'required|string|min:11|max:11',
+    //   'telefone2' => 'string|min:11|max:11',
+    //   'instituicao' => 'string|max:100',
+    //   'curso' => 'string|max:50',
+    //   'alunoUnivel' => 'required|int|min:1|max:1'
+    // ]);
 
     // Confere se o estado informado existe
-    $estado = Estado::where("UF", strtoupper($request->input("uf")))->get();
-    if(count($estado) == 1){
+    $estados = Estado::where("UF", strtoupper($request->input("uf")))->get();
+    foreach($estados as $estado){
 
       // Agora confere se a cidade informada existe
-      $cidade = Cidade::where([
+      $cidades = Cidade::where([
         "Cidade" => strtoupper($request->input("cidade")),
-        "Estado_Id" => $estado_id["Id"]
+        "Estado_Id" => $estado["Id"]
       ])->get();
-      if(count($cidade) > 0){
+      foreach($cidades as $cidade){
         $pessoa = new Pessoa;
-        $pessoa->cidade_id = $cidade["Cidade_Cod_Ibge"];
+        $pessoa->Cidade_Cod_Ibge = $cidade["Cod_Ibge"];
         $pessoa->Nome = $request->input("nome");
         $pessoa->Cpf = $request->input("cpf");
         $pessoa->Logradouro = $request->input("logradouro");
@@ -87,11 +87,7 @@ class InscricaoController extends Controller
         }
         $pessoa->AlunoUnivel = $request->input("alunoUnivel");
         $pessoa->save();
-      }else{
-
       }
-    }else{
-
     }
   }
 }
--- routes/web.php
+++ routes/web.php
@@ -7,18 +7,18 @@
 |
 | Here is where you can register web routes for your application. These
 | routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
+| contains the 'web' middleware group. Now create something great!
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
-Route::group(["prefix" => "api"], function(){
-  Route::group(["prefix" => "web"], function(){
-    Route::group(["prefix" => "inscricao"], function(){
-      Route::post("/new", "InscricaoController@postNew");
+Route::group(['prefix' => 'api'], function(){
+  Route::group(['prefix' => 'web'], function(){
+    Route::group(['prefix' => 'inscricao'], function(){
+      Route::post('/new', 'InscricaoController@postNew');
     });
   });
 });
