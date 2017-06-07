02703d660dce7daa7798f660857de03122517715
eduardohfreitas <duh.henrike@gmail.com>
2017-06-07 20:34:42 -0300

    atualizando o status do pagamento de acordo com o retorno

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -12,6 +12,32 @@ class PaymentController extends Controller
 
 		 foreach($payments as $payment){
 		 		$response = searchByReference($payment);
+		 		$payment->status = getStatus($response->status);
+		 }
+	}
+
+	/**
+		1	Aguardando pagamento: o comprador iniciou a transação, mas até o momento o PagSeguro não recebeu nenhuma informação sobre o pagamento.
+		2	Em análise: o comprador optou por pagar com um cartão de crédito e o PagSeguro está analisando o risco da transação.
+		3	Paga: a transação foi paga pelo comprador e o PagSeguro já recebeu uma confirmação da instituição financeira responsável pelo processamento.
+		4	Disponível: a transação foi paga e chegou ao final de seu prazo de liberação sem ter sido retornada e sem que haja nenhuma disputa aberta.
+		5	Em disputa: o comprador, dentro do prazo de liberação da transação, abriu uma disputa.
+		6	Devolvida: o valor da transação foi devolvido para o comprador.
+		7	Cancelada: a transação foi cancelada sem ter sido finalizada.
+	**/
+	private function getStatus(Integer $status){
+		switch ($status) {
+		 	case 1:
+		 		return 1;
+		 	case 2:
+		 		return 2;
+		 	case 3:
+		 	case 4:
+		 		return 3;
+		 	case 7:
+		 	 	return 0;
+		 	default:
+		 		return 1;
 		 }
 	}
 
