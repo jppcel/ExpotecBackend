12307f9dc7afd671bb65e4092db96be5b5b12828
João Paulo Polles <joaopaulopolles@gmail.com>
2017-06-12 12:43:46 -0300

    Criada a tabela de carrinho

---
+++ database/migrations/2017_06_12_124106_Create_Cart_Table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCartTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+      Schema::create('Cart', function (Blueprint $table) {
+          $table->increments("id");
+          $table->integer("Person_id")->unsigned();
+          $table->integer("Package_id")->unsigned();
+          $table->foreign("Package_id")->references("id")->on("Package");
+          $table->timestamps();
+      });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
