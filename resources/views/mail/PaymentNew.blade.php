505ace07694880425ace3d724fa3c8f41899426d
João Paulo Polles <joaopaulopolles@gmail.com>
2017-07-19 16:07:44 -0300

    app/Http/Controllers/InscricaoController.php
    Adicionado o envio do email para confirmar a criação da inscrição

---
+++ resources/views/mail/PaymentNew.blade.php
@@ -0,0 +1,8 @@
+<h3>{{env("APP_NAME")}} - Novo Pagamento</h3>
+<p>Olá {{$person_name}}!</p>
+<p>Viemos por esta apenas informar que o foi criado um pagamento para a inscrição #{{$subscription_id}}.</p>
+<p>Dados do pacote selecionado:</p>
+<ul>
+  <li>Nome do Pacote: {{$package_name}}</li>
+  <li>Preço do Pacote: {{$package_price}}</li>
+</ul>
