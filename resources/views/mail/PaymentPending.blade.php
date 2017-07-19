80226bb3674db72ed80a01dcbb214dbd3be5af2e
João Paulo Polles <joaopaulopolles@gmail.com>
2017-07-19 16:12:45 -0300

    app/Http/Controllers/PaymentController.php
    Inserido o pagamento pendente, e o envio de email quando tenta o retorno do pagseguro

---
+++ resources/views/mail/PaymentPending.blade.php
@@ -0,0 +1,3 @@
+<h3>{{env("APP_NAME")}} - Pagamento Pendente</h3>
+<p>Olá {{$person_name}}!</p>
+<p>Viemos por esta apenas informar que o pagamento para a inscrição #{{$subscription_id}} está pendente.</p>
