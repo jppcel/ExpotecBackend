8d8c02aefdb75f0b78ce772efc84c896b025e592
João Paulo Polles <joaopaulopolles@gmail.com>
2017-07-23 23:08:57 -0300

    Alterado para não dar problema de envio como expotec example

--- config/mail.php
+++ config/mail.php
@@ -56,8 +56,8 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'expotec@expotec.programatech.com'),
-        'name' => env('MAIL_FROM_NAME', 'Expotec Example'),
+        'address' => env('MAIL_FROM_ADDRESS'),
+        'name' => env('MAIL_FROM_NAME'),
     ],
 
     /*
